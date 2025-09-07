24174419f810a14f389d526fe5a4641a5a98cd52
PHMHoang <96359572+PHMHoang@users.noreply.github.com>
2025-09-07 17:31:43 +0700

    up

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from config.settings import settings
 from routers import admin, booking
-from flask_cors import CORS
-app = Flask(__name__)
-CORS(app)
+
 app = FastAPI()
 
 # Allow CORS for frontend
