45a77c9267da3b3dd7f3bc1bed43727362f0eec9
PHMHoang <96359572+PHMHoang@users.noreply.github.com>
2025-09-30 16:46:49 +0700

    up
    up

--- services/admin_service.py
+++
@@ -1,7 +0,0 @@
-from config.settings import settings
-from utils.security import verify_password
-
-class AdminService:
-    @staticmethod
-    def authenticate(username: str, password: str) -> bool:
-        return username == settings.ADMIN_USERNAME and verify_password(password, settings.ADMIN_PASSWORD_HASH)
